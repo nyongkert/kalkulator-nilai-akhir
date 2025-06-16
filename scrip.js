68ea2e197b49fdb5cea4076e04994c7eeefa1106
nyongkert <abselktalane@gmail.com>
2025-06-16 19:57:13 +0900

    Update scrip.js
    hapus file

--- scrip.js
+++ scrip.js
@@ -1,10 +1,1 @@
-document.getElementById("revealBtn").addEventListener("click", function () {
-  // Tampilkan elemen jumpscare
-  document.getElementById("jumpscare").classList.remove("hidden");
 
-  // Mainkan video dan suara jumpscare
-  const video = document.getElementById("scareVideo");
-  video.play();
-
-  // Tambahkan efek suara keras (kalau videonya nggak ada audio, bisa tambahkan audio eksternal)
-});
